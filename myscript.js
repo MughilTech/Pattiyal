18ded6fab44755dec152ab3458beb814e8321211
Ramkumar <ramumm@thoughtworks.com>
2012-07-11 02:37:35 +0530

    ram | shortcut for nav and search + removed left col

--- myscript.js
+++ myscript.js
@@ -2,18 +2,16 @@ $(document).ready(function(){
 	$("h3.r").each(function(index,element){
  	go_button = $('<input type="button" value="Go"></input>');
  	go_button.click(function(e){
- 		console.log(e.target);
  		frameIt($('h3.r a', e.target.parentNode)[0].href);
  	});
  	$(element).after(go_button);
  });
-   var some = new MutationSummary({ callback: handleSummary, 
+   var searchLink = new MutationSummary({ callback: handleSearchLinks, 
    	queries: [{ element: "h3.r" }]});
-	
+
 	var stylesheetDoc = document.styleSheets[0];
 	if (stylesheetDoc != null)
 		setScrollbarStyle(stylesheetDoc);
-	
 });
 
 function setScrollbarStyle(stylesheetDoc){
@@ -23,7 +21,37 @@ function setScrollbarStyle(stylesheetDoc){
 	stylesheetDoc.insertRule('::-webkit-scrollbar-thumb:window-inactive {background: rgba(0,0,0,0.4); }',3);
 }
 
-function handleSummary(e)
+function handleLeftNav()
+{
+    $('#leftnavc')[0].setAttribute("hidden");	
+	$('#center_col').css('margin-left', '0px');
+	
+}
+
+function addShortcuts()
+{
+	shortcut.add('s', function(){
+		$('#gbqfq').focus();
+		 $('#gbqfq').select();
+	});
+	shortcut.add('tab', function(){
+		
+			var currentSelection = $('.vsc.pattiyal:first');
+			var searchList = $('.vsc');
+			var toBeHighlighted = $('.vsc:first');
+			if(currentSelection.length != 0)
+			{
+				currentSelection.removeClass('pattiyal');
+				currentSelection.css('background-color', 'white');
+				var nextIndex = jQuery.inArray(currentSelection[0], searchList) + 1;
+				toBeHighlighted = $(searchList[nextIndex]);
+			}
+			toBeHighlighted.addClass('pattiyal');
+			toBeHighlighted.css('background-color', 'grey');
+	});
+}
+
+function handleSearchLinks(e)
 {
  $(e[0].added).each(function(index,element){
  	go_button = $('<input type="button" value="Go"></input>');
@@ -33,12 +61,14 @@ function handleSummary(e)
  	});
  	$(element).after(go_button);
  });
+ $('div.vspib').remove();
 }
 
 function frameIt(url)
 {
-	
 	if($('#pattiyal_frame').length == 0){
+		handleLeftNav();
+		addShortcuts();
 		var center_col = $('#center_col');
 		var cnt_width = $('#cnt').width();
 		var rcnt_width = $('#rcnt').width();
@@ -51,7 +81,7 @@ function frameIt(url)
 		center_col.css("float", "left");
 
 		center_col.after('<div id="pattiyal_frame">\
-			<iframe src="'+url+'" style="width:'+frame_width.toString()+'px;height:'+frame_height+'; margin-left:-'+frame_left+'"></iframe></div>');
+			<iframe src="'+url+'" style="width:'+frame_width.toString()+'px;height:'+frame_height+'; margin-left:-'+frame_left.toString()+'"></iframe></div>');
 	}
 	else
 		$('#pattiyal_frame iframe')[0].src = url;
